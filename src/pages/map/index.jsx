aa7bbd1b45a3bd2d56d05fdf55f06f2638dc166e
JUNJIE WANG <>
2022-03-28 21:18:32 +0800

    update

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,12 +1,44 @@
 import './index.css';
+import { Input } from 'antd';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 
 const MapPage = () => {
-    const height = window.innerHeight;
+    function callback(key) {
+        console.log(key);
+    }
 
     return (
-        <section className="map-page" style={{ height: height }}>
+        <section className="map-page">
             <div className='header'>
+                <Input
+                    placeholder='Choose Starting Point' />
+                <Input
+                    placeholder='Choose Ending Point' />
+            </div>
+            <div className='result-box'>
+                <div className='list'>
+                    <div>
 
+                    </div>
+                    <div>
+                        <Tabs defaultActiveKey="1" onChange={callback}>
+                            <TabPane tab="Tab 1" key="1">
+                                Content of Tab Pane 1
+                            </TabPane>
+                            <TabPane tab="Tab 2" key="2">
+                                Content of Tab Pane 2
+                            </TabPane>
+                            <TabPane tab="Tab 3" key="3">
+                                Content of Tab Pane 3
+                            </TabPane>
+                        </Tabs>
+                    </div>
+                </div>
+                <div className='google-map'>
+
+                </div>
             </div>
         </section>
     );
